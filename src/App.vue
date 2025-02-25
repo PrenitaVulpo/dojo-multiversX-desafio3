59967804320ce590e9fc6433e70fd5071ff29d3a
Luan-Web3 <luansilvalemes.web3@gmail.com>
2025-02-25 17:06:06 -0300

    feat: implement gemini

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,20 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
+import { generateContent } from './utils/gemini'
+import { onMounted, ref } from 'vue'
+
+const prompt = ref("")
+
+const generateByPrompt = async () => {
+  const content = await generateContent(prompt.value)
+  console.log({ content })
+}
 </script>
 
 <template>
   <div id="prompt-chat"></div>
-  <input type="text" name="prompt" id="prompt">
+  <input v-model="prompt" type="text" name="prompt" id="prompt">
+  <button @click="generateByPrompt">Pesquisar</button>
 </template>
 
 <style scoped>
@@ -14,9 +24,11 @@ import HelloWorld from './components/HelloWorld.vue'
   will-change: filter;
   transition: filter 300ms;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
